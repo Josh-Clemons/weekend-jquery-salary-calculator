44ea9e3bf531236a934a56ceb1485a0b14683828
Josh Clemons <mrjoshc@gmail.com>
2022-10-27 21:05:00 -0500

    completed remove function

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function onReady () {
     // add employee when 'Submit' button is clicked
     // remove employee when corresponding 'delete' button is clicked
     $('#submitBtn').on('click', addEmployee)
+    $('#employeeTableBody').on('click', '.removeBtn', removeEmployee)
 
     // state actions:
     // add or remove employee information from employees object
@@ -21,7 +22,6 @@ function onReady () {
     // refresh employees table based on object
     // clear input fields
 
-
     render();
 }
 
@@ -30,16 +30,32 @@ function addEmployee () {
     let employee = {
         fName: $('#fNameInput').val(),
         lName: $('#lNameInput').val(),
+        title: $('#titleInput').val(),
         id: $('#idInput').val(),
         annualSalary: $('#annualSalaryInput').val(),
         dataID: idCount
     }
     idCount ++;
     employees.push(employee);
+
+    $('#fNameInput').val('');
+    $('#lNameInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#annualSalaryInput').val('');
+
     render ();
 } // end addEmployee
 
-
+function removeEmployee() {
+    // console.log('in remove');
+    for (let [i, employee] of employees.entries()) {
+        if (employee.dataID == $(this).attr('data-id')) {
+            employees.splice(i, 1);
+        }
+    }
+    render ();
+}
 
 function render() {
     $('#employeeTableBody').empty();
@@ -47,8 +63,12 @@ function render() {
         $('#employeeTableBody').append (`
             <tr>
                 <td>${employee.fName}</td>
-        
-        
+                <td>${employee.lName}</td>
+                <td>${employee.id}</td>
+                <td>${employee.title}</td>
+                <td>${employee.annualSalary}</td>
+                <td><button class="removeBtn" data-id="${employee.dataID}">Delete</button></td>
+            </tr>
         `)
     }
 }
